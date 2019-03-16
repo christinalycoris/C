2688fef480868ebb6d4558a6ced7dff4d2691416
Christina <48631564+christinalycoris@users.noreply.github.com>
2019-03-16 14:10:05 -0700

    Create if.c

---
+++ if.c
@@ -0,0 +1,26 @@
+#include <stdio.h>	
+
+int main()
+{
+    int grade;
+
+    printf( "Please enter your grade: " );
+    scanf( "%d", &grade );
+    if ( grade < 60 ) {
+        printf ("You have a F in this course.\t :(\n" );
+    }
+    else if ( grade < 70 ) {
+        printf( "You have a D in this course.\n" ); 
+    }
+    else if ( grade < 80 ) {
+        printf( "You have a C in this course.\n" );  
+    }
+    else if ( grade < 90 ) {
+        printf( "You have a B in this course.\n" );       
+    }
+    else {
+        printf( "You have a A in this course.\t :)\n" );
+    }
+  return 0;
+}
+
