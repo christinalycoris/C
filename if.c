0f6650c97905a5190b6bc4a2b41b83d27afd756e
Christina <48631564+christinalycoris@users.noreply.github.com>
2019-03-16 14:15:21 -0700

    Update if.c

--- if.c
+++ if.c
@@ -6,21 +6,23 @@ int main()
 
     printf( "Please enter your grade: " );
     scanf( "%d", &grade );
+    
     if ( grade < 60 ) {
-        printf ("You have a F in this course.\t :(\n" );
+        printf ("You have a 'F' in this course.\t :(\n" );
     }
     else if ( grade < 70 ) {
-        printf( "You have a D in this course.\n" ); 
+        printf( "You have a 'D' in this course.\n" ); 
     }
     else if ( grade < 80 ) {
-        printf( "You have a C in this course.\n" );  
+        printf( "You have a 'C' in this course.\n" );  
     }
     else if ( grade < 90 ) {
-        printf( "You have a B in this course.\n" );       
+        printf( "You have a 'B' in this course.\n" );       
     }
     else {
-        printf( "You have a A in this course.\t :)\n" );
+        printf( "You have an 'A' in this course.\t :)\n" );
     }
+    
   return 0;
 }
 
