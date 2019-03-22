f389e1bba36e1e7e3b5b7c015a3e775351b4cbc4
Christina <48631564+christinalycoris@users.noreply.github.com>
2019-03-22 01:07:36 -0700

    Update for.c

--- for.c
+++ for.c
@@ -3,8 +3,11 @@
 int main () 
 {
 
-    int i,count;
+    int i,count,sum;
     int x,y;
+    
+    count=0;
+    sum=0;
 
     printf("Input X: "); 
     scanf("%d", &x);
@@ -13,13 +16,14 @@ int main ()
 
     for( i = x; i <= y; i++ ){
         printf("The value of X is %d\n", x);
+        sum=sum+x;
         x++;
         count++;
     }
     
     printf("Loop ran %d times.\n", count);
+    printf("Sum of all integers is %d.\n", sum);
     
 
     return 0;
 }
-
