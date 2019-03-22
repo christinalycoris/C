816fda216382d11218388db3590c9625f16abd3d
Christina <48631564+christinalycoris@users.noreply.github.com>
2019-03-21 20:30:15 -0700

    Update a+b.c

--- a+b.c
+++ a+b.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-int main()
-{
-    int a,b;
-    
-    a=3;
-    b=2;
-    
-    printf("%d", a + b);
+int main() {
+	int a,b;
 
-    return 0;
+	a = 3;
+	b = 2;
+
+	printf("%d", a + b);
+
+	return 0;
 }
 
